2fafe0265e84f6a85c0831b5094f2606c8759858
AnabelaSilva <up201506034@fe.up.pt>
2017-11-02 16:44:14 +0000

    Joga com jogadas simples

--- logic/ask_for_input.pl
+++ logic/ask_for_input.pl
@@ -2,26 +2,70 @@
 Jogador escolhe a peça que vai jogar
 e o sentido do movimento da mesma
 */
-selectPiece(CurrBoard, NewBoard, PlayerNum):-
+selectPiece(CurrBoard, NewBoard, 1):-
+  showBoard(CurrBoard),
+repeat,
+  write('Choose your piece.'), nl,
+  write('Row (Number)'), read(Y),
+  write('Column (Letter)'), read(L),convertLetterToNum(L, X),
+  findPiece(CurrBoard, X-Y, Player),
+  !,
+  member(Player, [1, 11]), !, defineDirection(CurrBoard, NewBoard, X-Y, 1, Player).
+
+selectPiece(CurrBoard, NewBoard, 2):-
   showBoard(CurrBoard),
 repeat,
   write('Choose your piece.'), nl,
   write('Row (Number)'), read(Y),
   write('Column (Letter)'), read(L), convertLetterToNum(L, X),
-  findPiece(CurrBoard, X-Y, Player), !,%TESTE
-  PlayerNum = 1 -> (member(Player, [1, 11]), !, defineDirection(CurrBoard, NewBoard, X-Y, PlayerNum, Player));
-  member(Player, [2, 22]), !, write('NEW'),defineDirection(CurrBoard, NewBoard, X-Y, PlayerNum, Player).
+  findPiece(CurrBoard, X-Y, Player),
+  !,
+  member(Player, [2, 22]), !, defineDirection(CurrBoard, NewBoard, X-Y, 2, Player).
 
-defineDirection(CurrBoard, NewBoard, X-Y, PlayerNum, Player):-
+
+
+/*
+selectPiece(CurrBoard, NewBoard, PlayerNum):-
+  showBoard(CurrBoard),
+repeat,
+  write('Choose your piece.'), nl,
+  write('Row (Number)'), read(Y),
+  write('Column (Letter)'), read(L),write('L :'),write(L),nl, convertLetterToNum(L, X),
+ write('X :'),write(X),nl, write('Y :'),write(Y),nl, 
+ findPiece(CurrBoard, X-Y, Player),
+!,%TESTE
+write('saX :'),write(X),nl, write('asxdY :'),write(Y),nl,
+  PlayerNum = 1 -> (member(Player, [1, 11]), !, write('P1X :'),write(X),nl, write('Y :'),write(Y),nl,defineDirection(CurrBoard, NewBoard, X-Y, PlayerNum, Player));
+  (member(Player, [2, 22]), !,write('AQUI'),write('X :'),write(X),nl, write('Y :'),write(Y),nl,defineDirection(CurrBoard, NewBoard, X-Y, PlayerNum, Player)).
+*/
+
+
+defineDirection(CurrBoard, NewBoard, X-Y, 1, Player):-
   write('Move the piece.'), nl,
   read(D),
+  write('Move player 1'),nl,
+  validMan1Move(CurrBoard, X-Y, NewX-NewY, Player, D),
+  updateBoardSimpleMove(CurrBoard, NewBoard, X-Y, NewX-NewY, Player),
+  showBoard(NewBoard).
+defineDirection(CurrBoard, NewBoard, X-Y, 2, Player):-
+  write('Move the piece.'), nl,
+  read(D),
+  write('Move player 2'), nl,
+  validMan2Move(CurrBoard, X-Y, NewX-NewY, Player, D),
+  updateBoardSimpleMove(CurrBoard, NewBoard, X-Y, NewX-NewY, Player),
+  showBoard(NewBoard).
+
+/*
+defineDirection(CurrBoard, NewBoard, X-Y, PlayerNum, Player):-
+  write('Move the piece.'), nl,
+  read(D),nl, write('X :'),write(X),nl, write('Y :'),write(Y),nl,write('D :'),write(D),nl, 
 %  repeat,
   Player = 1 ->
      (write('Move player 1'),nl,validMan1Move(CurrBoard, X-Y, NewX-NewY, Player, D), updateBoardSimpleMove(CurrBoard, NewBoard, X-Y, NewX-NewY, Player),
      showBoard(NewBoard));
  % Player = 2 ->
-      (write('Move player 2'),nl,validMan2Move(CurrBoard, X-Y, NewX-NewY, Player, D), updateBoardSimpleMove(CurrBoard, NewBoard, X-Y, NewX-NewY, Player),
-      showBoard(NewBoard)).%, CurrBoard is NewBoard;
+      (write('Move player 2'),validMan2Move(CurrBoard, X-Y, NewX-NewY, Player, D), updateBoardSimpleMove(CurrBoard, NewBoard, X-Y, NewX-NewY, Player),
+      showBoard(NewBoard)).%, CurrBoard is NewBoard;*/
 /*  repeat,
   read(D),
   member(D, [1, 2, 3, 4, 6, 7, 8, 9]), !,
