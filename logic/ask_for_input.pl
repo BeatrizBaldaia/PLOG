1e062ccd1ce5d92701f7d161d9376324bbddf510
AnabelaSilva <up201506034@fe.up.pt>
2017-11-03 10:20:48 +0000

    Adicionar ite.

--- logic/ask_for_input.pl
+++ logic/ask_for_input.pl
@@ -22,26 +22,8 @@ repeat,
   !,
   member(Player, [2, 22]), !, defineDirection(CurrBoard, NewBoard, X-Y, Player).
 
-
-
-/*
-selectPiece(CurrBoard, NewBoard, PlayerNum):-
-  showBoard(CurrBoard),
-repeat,
-  write('Choose your piece.'), nl,
-  write('Row (Number)'), read(Y),
-  write('Column (Letter)'), read(L),write('L :'),write(L),nl, convertLetterToNum(L, X),
- write('X :'),write(X),nl, write('Y :'),write(Y),nl, 
- findPiece(CurrBoard, X-Y, Player),
-!,%TESTE
-write('saX :'),write(X),nl, write('asxdY :'),write(Y),nl,
-  PlayerNum = 1 -> (member(Player, [1, 11]), !, write('P1X :'),write(X),nl, write('Y :'),write(Y),nl,defineDirection(CurrBoard, NewBoard, X-Y, PlayerNum, Player));
-  (member(Player, [2, 22]), !,write('AQUI'),write('X :'),write(X),nl, write('Y :'),write(Y),nl,defineDirection(CurrBoard, NewBoard, X-Y, PlayerNum, Player)).
-*/
-
-
 defineDirection(CurrBoard, NewBoard, X-Y, Player):-
-  member(Player, [1, 2]),
+  member(Player, [1, 2]), !,
   write('Move the piece.'), nl,
   read(D),
 ite(
@@ -70,49 +52,7 @@ keepMovingKing(CurrBoard, FinalBoard, X-Y, Player, D):-
 	keepMoving(D),
 	keepMovingKing(NewBoard, FinalBoard, NewX-NewY, Player, D),
 	FinalBoard = NewBoard).
-/*
-defineDirection(CurrBoard, NewBoard, X-Y, 1, 11):-
-  write('Move the piece.'), nl,
-  read(D),
-  member(D, [1, 2, 3, 4, 6, 7, 8, 9]), !,
-  write('Move player 1'),nl,
-  validKing1Move(CurrBoard, X-Y, NewX-NewY, 11, D),
-  updateBoardSimpleMove(CurrBoard, NewBoard, X-Y, NewX-NewY, 11),
-  showBoard(NewBoard),
-  keepMoving(NewX-NewY, 11, Direction).
-defineDirection(CurrBoard, NewBoard, X-Y, 2, 22):-
-  write('Move the piece.'), nl,
-  read(D),
-  member(D, [1, 2, 3, 4, 6, 7, 8, 9]), !,
-  write('Move player 2'), nl,
-  validKing2Move(CurrBoard, X-Y, NewX-NewY, Player, D),
-  updateBoardSimpleMove(CurrBoard, NewBoard, X-Y, NewX-NewY, Player),
-  showBoard(NewBoard)),
-  keepMoving(NewX-NewY, Player, Direction).
-*/
 
-/*
-defineDirection(CurrBoard, NewBoard, X-Y, PlayerNum, Player):-
-  write('Move the piece.'), nl,
-  read(D),nl, write('X :'),write(X),nl, write('Y :'),write(Y),nl,write('D :'),write(D),nl, 
-repeat,
-Player = 1 ->
-     (write('Move player 1'),nl,validMan1Move(CurrBoard, X-Y, NewX-NewY, Player, D), updateBoardSimpleMove(CurrBoard, NewBoard, X-Y, NewX-NewY, Player),
-     showBoard(NewBoard));
-Player = 2 ->
-      (write('Move player 2'),validMan2Move(CurrBoard, X-Y, NewX-NewY, Player, D), updateBoardSimpleMove(CurrBoard, NewBoard, X-Y, NewX-NewY, Player),
-      showBoard(NewBoard));%, CurrBoard is NewBoard;
-repeat,
-  read(D),
-  member(D, [1, 2, 3, 4, 6, 7, 8, 9]), !,
-  repeat,
-  Player = 11 ->
-    validKing1Move(CurrBoard, X-Y, NewX-NewY, Player, D), updateBoardSimpleMove(CurrBoard, NewBoard, X-Y, NewX-NewY, Player),
-    showBoard(NewBoard), CurrBoard is NewBoard, keepMoving(NewX-NewY, Player, Direction);
-  Player = 22 ->
-    validKing2Move(CurrBoard, X-Y, NewX-NewY, Player, D), updateBoardSimpleMove(CurrBoard, NewBoard, X-Y, NewX-NewY, Player),
-    showBoard(NewBoard), CurrBoard is NewBoard, keepMoving(NewX-NewY, Player, Direction).
-*/
 /*
 O rei pode andar mais do que uma
 casa na mesma linha de percurso
@@ -122,12 +62,4 @@ keepMoving(Direction):-
   D = Direction ->  true;%para continuar a mover no ciclo repeat
   D \= '\n' -> keepMoving(Direction);%introduziu a direcao errada
   fail.
-/*
-keepMoving(X-Y, Player, Direction):-
-  read(D),
-  D = Direction.%WAHT
 
-keepMoving(X-Y, Player, Direction):-
-  read(D),
-  D \= Direction, fail.
-*/
---
+++ utils/aux_func.pl
@@ -0,0 +1,4 @@
+ite(If, Then, _Else):- 
+	If, !, Then.
+ite(_If, _Then, Else):-
+	Else.
