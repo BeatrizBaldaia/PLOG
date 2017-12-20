4ec179d4730a4cba6cec9615235566d03960bb63
AnabelaSilva <up201506034@fe.up.pt>
2017-12-20 15:42:57 +0000

    Pretty.

--- trabalho 2/logic/print.pl
+++ trabalho 2/logic/print.pl
@@ -1,20 +1,63 @@
+
+%['trace_rode.pl'],calculate_road([2-2-5,5-2-5,3-3-3,2-5-3,6-6-2]-[5-4],6,RES),showBoard(RES,[2-2-5,5-2-5,3-3-3,2-5-3,6-6-2]-[5-4],6).
 /*Predicado Para Mostrar o Tabuleiro*/
-showBoard(X):- !,nl,showRow(X, 1).
-showRow(X, Y):- X = [],!.
-showRow(X, Y):- !,X = [H|R],
+showBoard(Solution,Board-Zeros,Dim):- !,createPrintBoard(Solution,Board-Zeros,Dim,X),nl,showRow(X, 1,Dim).
+showRow(X, Y,Dim):- X = [],!.
+showRow(X, Y,Dim):- !,X = [H|R],
 	Yn is (Y + 1),
-	write('--------------------------------------------------------------------------------------------'), nl,
+	writeSlash(Dim),
+	%write('----'), nl,
+	nl,
 	showRowValues(H), nl,
-	showRow(R, Yn).
+	showRow(R, Yn,Dim).
 showRowValues(X):- X = [] ,!, write(' |').
 showRowValues(X):- !,X = [H|R],
 	write(' | '),
 	showPiece(H),
 	showRowValues(R).
-showPiece(X):- X = 1,!,
-	put_code(219).
-showPiece(X):- X = 0,!,
+showPiece(X):- X = 11,!,
+	put_code(9619).%9215).%219
+showPiece(X):- X = 10,!,
 	write(' ').
-showPiece(X):- !,X = 2,
-  	put_code(65).
-%showPiece(X):- put_code(66).
+showPiece(X):- X = 9,!,
+	%('A').
+	put_code(9209).
+showPiece(X):- write(X).
+writeSlash(0).
+writeSlash(Dim):-
+	write('----'),
+	ND is Dim - 1,
+	writeSlash(ND).
+createPrintBoard(Solution,Clues-Zeros,Dim,Square):-
+	length(Square, Dim),
+  createPBoard(Square, Dim),
+	write('ZEROS'),nl,
+	putZeros(Square,Zeros),
+	write('Clues'),nl,
+	putClues(Square,Clues),
+	write('Pata'),nl,
+	putPath(Solution,Square).
+
+putPathLin([],[]).
+putPathLin([S|LinSol],[Q|LinSqu]):-
+	ite(nonvar(Q),true,(Elem is S + 10,Q is Elem)),
+	putPathLin(LinSol,LinSqu).
+putPath([],[]).
+putPath([LinSol|Solution],[LinSqu|Square]):-
+	putPathLin(LinSol,LinSqu),
+	putPath(Solution,Square).
+putClues(_,[]).
+putClues(Square,[X-Y-V|Clues]):-
+	nth1(Y,Square,Lin),
+	nth1(X,Lin,V),
+	putClues(Square,Clues).
+putZeros(_,[]).
+putZeros(Square,[X-Y|Zeros]):-
+	nth1(Y,Square,Lin),
+	nth1(X,Lin,9),
+	putZeros(Square,Zeros).
+createPBoard([H],Dim):-
+	length(H, Dim).
+createPBoard([H|Answer], Dim):-
+	length(H, Dim),
+	createPBoard(Answer, Dim).
