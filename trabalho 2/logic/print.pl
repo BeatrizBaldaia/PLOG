2fcac790162e0b13737580c70c4d85b8f0f5c818
BeatrizBaldaia <bibibaldaia@hotmail.com>
2017-12-21 16:45:33 +0000

    apaguei comentarios

--- trabalho 2/logic/print.pl
+++ trabalho 2/logic/print.pl
@@ -3,31 +3,33 @@
 /*Predicado Para Mostrar o Tabuleiro*/
 showBoard(Solution,Board-Zeros,Dim):- !,createPrintBoard(Solution,Board-Zeros,Dim,X),nl,showRow(X, 1,Dim).
 showRow(X, Y,Dim):- X = [],!.
-showRow(X, Y,Dim):- !,X = [H|R],
+showRow(X, Y,Dim):- !, X = [H|R],
 	Yn is (Y + 1),
 	writeSlash(Dim),
-	%write('----'), nl,
 	nl,
 	showRowValues(H), nl,
 	showRow(R, Yn,Dim).
+
 showRowValues(X):- X = [] ,!, write(' |').
 showRowValues(X):- !,X = [H|R],
 	write(' | '),
 	showPiece(H),
 	showRowValues(R).
+
 showPiece(X):- X = 11,!,
-	put_code(9619).%9215).%219
+	put_code(9619).
 showPiece(X):- X = 10,!,
 	write(' ').
 showPiece(X):- X = 9,!,
-	%('A').
 	put_code(9209).
 showPiece(X):- write(X).
+
 writeSlash(0):-!.
 writeSlash(Dim):-
 	write('----'),
 	ND is Dim - 1,
 	writeSlash(ND).
+
 createPrintBoard(Solution,Clues-Zeros,Dim,Square):-
 	length(Square, Dim),
   createPBoard(Square, Dim),
@@ -39,15 +41,18 @@ putPathLin([],[]).
 putPathLin([S|LinSol],[Q|LinSqu]):-
 	ite(nonvar(Q),true,(Elem is S + 10,Q is Elem)),
 	putPathLin(LinSol,LinSqu).
+
 putPath([],[]).
 putPath([LinSol|Solution],[LinSqu|Square]):-
 	putPathLin(LinSol,LinSqu),
 	putPath(Solution,Square).
+
 putClues(_,[]).
 putClues(Square,[X-Y-V|Clues]):-
 	nth1(Y,Square,Lin),
 	nth1(X,Lin,V),
 	putClues(Square,Clues).
+	
 putZeros(_,[]).
 putZeros(Square,[X-Y|Zeros]):-
 	nth1(Y,Square,Lin),
